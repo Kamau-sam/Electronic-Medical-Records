430a92a1bae064bbe86d2fae8053f0ca12f4e819
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 11:56:19 +0300

    Added comments to the  doctor model

--- models/doctor.py
+++ models/doctor.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from .base import Base
 
+# Define the Doctor model class
 class Doctor(Base):
     __tablename__ = 'doctors'
     doctor_id = Column(Integer, primary_key=True)
@@ -10,5 +11,6 @@ class Doctor(Base):
     contact_info = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
 
+  # Relationship to the  Appointment AND MedicalRecord model
     appointments = relationship('Appointment', back_populates='doctor')
     medical_records = relationship('MedicalRecord', back_populates='doctor')
