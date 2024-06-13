687aeec732ff2e2056e22094a3ccbdf4cf646e23
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 11:43:01 +0300

    Restructured the appointment model  to improve readability

--- models/appointment.py
+++ models/appointment.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import Base
 
+
+# Define the Appointment model class
 class Appointment(Base):
     __tablename__ = 'appointments'
     appointment_id = Column(Integer, primary_key=True)
@@ -11,6 +13,7 @@ class Appointment(Base):
     time = Column(Time, nullable=False)
     reason = Column(String, nullable=False)
 
+#Relationship to the Patient , doctor, and medical record model
     patient = relationship('Patient', back_populates='appointments')
     doctor = relationship('Doctor', back_populates='appointments')
     medical_record = relationship('MedicalRecord', uselist=False, back_populates='appointment')
