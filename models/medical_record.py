f765840d35f97083918ee65372b690766642f5de
Kamau-sam <skamau.6411@gmail.com>
2024-06-13 11:58:48 +0300

    improved readability of the mediicaal_record model

--- models/medical_record.py
+++ models/medical_record.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import Base
 
+# Define the MedicalRecord model class
 class MedicalRecord(Base):
     __tablename__ = 'medical_records'
     record_id = Column(Integer, primary_key=True)
@@ -12,6 +13,7 @@ class MedicalRecord(Base):
     treatment = Column(String, nullable=False)
     notes = Column(String)
 
+    #Relationships
     patient = relationship('Patient', back_populates='medical_records')
     doctor = relationship('Doctor', back_populates='medical_records')
     appointment = relationship('Appointment', back_populates='medical_record')
